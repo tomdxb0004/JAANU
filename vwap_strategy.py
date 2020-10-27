b7b4d4d37d7d0f2857553f6e87129d57dc0ac710
TOM JOSEPH <31935042+tomdxb0004@users.noreply.github.com>
2020-10-28 01:58:58 +0400

    vwap_orb

--- vwap_strategy.py
+++ vwap_strategy.py
@@ -11,6 +11,8 @@ from table_to_html import send_dataframe
 import shutil
 warnings.filterwarnings("ignore")
 import time
+ 
+
 time_list = time.ctime().split(' ')
 date = time_list[1] + '-' + time_list[2] +'-' + time_list[4]
 os.chdir("C:\\Users\\tomdx\\Documents\\GitHub\\stock")
@@ -50,9 +52,15 @@ def get_nse100_data(stock_dict=stock_dict):
     return df
 
 for i in range(9):
+    print('************************************ Scraping Started at: ',time.ctime())
     get_nse100_data()
-    time.sleep(240)
+    print('****************************Completed',i,'out of ',8,'******************************',time.ctime())
 
+    if i == 8:
+        break
+    else: 
+        time.sleep(260)
+print('-----------------------------------Calculating VWAP---------------------------------------------',time.ctime())
 appended_df = pd.DataFrame()
 path = r'./vwap_data/' 
 all_files = glob.glob(path + "/*.csv")
@@ -70,6 +78,9 @@ def vwap(infy):
     infy.loc[infy['vwap']>infy['LAST TRADED PRICE'],'Action'] = 'Long'
     infy.loc[infy['vwap']<infy['LAST TRADED PRICE'],'Action'] = 'Short'
     infy.loc[infy['vwap']==infy['LAST TRADED PRICE'],'Action'] = 'No VWAP'
+    infy['vwap_diff_as_percent_of_LTP'] = round(((infy['vwap'] - infy['LAST TRADED PRICE'])/infy['LAST TRADED PRICE'])*100,2)
+    infy.sort_values('vwap_diff_as_percent_of_LTP',ascending=False,inplace=True)
+    infy.drop(columns=['avg','price_volume','pv_cum_sum','cum_v'],inplace=True)
     return infy
 
 final_df = pd.DataFrame()
@@ -84,10 +95,24 @@ trend = float(nifty50['pChange'])
 
 if trend>0:
     buy_candidates = final_df[final_df['Action']=='Long']
-    send_dataframe(buy_candidates,'green_light')
+    buy_candidates.loc[(buy_candidates['OPEN'] < buy_candidates['LAST TRADED PRICE']),'Action'] = 'Long_ORB_VWAP'
+    buy_candidates = buy_candidates[buy_candidates['Action']=='Long_ORB_VWAP'].head(7)
+
+    if len(buy_candidates) !=0:
+        send_dataframe(buy_candidates,'green_light')
+    else:
+        buy_candidates = pd.DataFrame('No candidates to BUY as per combined ORB-VWAP strategy')
+        send_dataframe(buy_candidates,'green_light')
 else:
     sell_candidates = final_df[final_df['Action']=='Short']
-    send_dataframe(sell_candidates,'red_light')
+    sell_candidates.loc[(sell_candidates['LOW'] > sell_candidates['LAST TRADED PRICE']),'action'] = 'Short_ORB_VWAP'
+    sell_candidates = sell_candidates[sell_candidates['Action']=='Short_ORB_VWAP'].head(7)
+
+    if len(sell_candidates) !=0:
+        send_dataframe(sell_candidates,'red_light')
+    else:
+        sell_candidates = pd.DataFrame('No candidates to SHORT as per combined ORB-VWAP strategy')
+        send_dataframe(sell_candidates,'red_light')
 
 path = "C:\\Users\\tomdx\\Documents\\GitHub\\stock\\daily_closing_data\\"
 file_name = path + date + 'vwap_df.csv'
